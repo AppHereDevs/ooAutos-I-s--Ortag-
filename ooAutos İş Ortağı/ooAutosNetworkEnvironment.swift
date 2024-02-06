9eb0e9583e5e03c63da320abac4aa4bdcd46ef44
Arda Onat <ardonat97@gmail.com>
2024-02-06 10:14:32 +0000

    Add app icon, change base url, change minimum target to 15.

--- ooAutos İş Ortağı/ooAutosNetworkEnvironment.swift
+++ ooAutos İş Ortağı/ooAutosNetworkEnvironment.swift
@@ -1,4 +1,5 @@
 import CoreModule
+import Foundation
 
 enum ooAutosNetworkEnvironment: NetworkEnvironment {
     case prod
@@ -6,8 +7,49 @@ enum ooAutosNetworkEnvironment: NetworkEnvironment {
 
     var baseURL: String {
         switch self {
-        case .prod: return "https://testwa.ooautos.com/api/mobile"
-        case .rest: return "https://testwa.ooautos.com/rest/mobile"
+        case .prod:
+            if Config.appConfiguration == .TestFlight || Config.appConfiguration == .Debug {
+                return "https://testwa.ooautos.com/api/mobile"
+            } else {
+                return "https://wa.ooautos.com/api/mobile"
+            }
+
+        case .rest:
+            if Config.appConfiguration == .TestFlight || Config.appConfiguration == .Debug {
+                return "https://testwa.ooautos.com/rest/mobile"
+            } else {
+                return "https://wa.ooautos.com/rest/mobile"
+            }
+        }
+    }
+}
+
+enum AppConfiguration {
+    case Debug
+    case TestFlight
+    case AppStore
+}
+
+enum Config {
+    // This is private because the use of 'appConfiguration' is preferred.
+    private static let isTestFlight = Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
+
+    // This can be used to add debug statements.
+    static var isDebug: Bool {
+        #if DEBUG
+            return true
+        #else
+            return false
+        #endif
+    }
+
+    static var appConfiguration: AppConfiguration {
+        if isDebug {
+            return .Debug
+        } else if isTestFlight {
+            return .TestFlight
+        } else {
+            return .AppStore
         }
     }
 }
